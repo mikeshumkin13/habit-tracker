c735bf6af0f612dd58d141cce0d8ed14607cb81a
a12345 <shu.m2010@yandex.ru>
2025-07-21 13:16:20 +0300

    Добавлена регистрация пользователя через /api/users/register/

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/habits/', include('habits.urls')),
+    path('api/users/', include('users.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
