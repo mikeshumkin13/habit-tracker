9d7cca014e99caec6afe47bf8cc777fe5c24a636
a12345 <shu.m2010@yandex.ru>
2025-07-21 12:41:17 +0300

    Настроены пагинация и JWT-авторизация

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/habits/', include('habits.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
