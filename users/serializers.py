ce1f9284642f59ddfd9ae64d56ba95dab010b843
a12345 <shu.m2010@yandex.ru>
2025-07-22 12:17:19 +0300

    Обновлён RegisterAPIView с поддержкой get_user_model()

--- users/serializers.py
+++ users/serializers.py
@@ -1,19 +1,18 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
 
     class Meta:
         model = User
-        fields = ("username", "password")
+        fields = ("email", "password")
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data["username"],
-            password=validated_data["password"]
-        )
+        user = User.objects.create_user(**validated_data)
         return user
 
 
--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics, permissions
 from .serializers import RegisterSerializer
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
-class RegisterView(generics.CreateAPIView):
+class RegisterAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [permissions.AllowAny]
+
 
 
