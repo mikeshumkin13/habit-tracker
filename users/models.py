ac16e4e4ba8406e6446b9eb517964b4a967dd2e6
a12345 <shu.m2010@yandex.ru>
2025-07-25 16:23:12 +0300

    Восстановлен импорт JWT Token Views в config/urls.py, выполнены миграции

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class TelegramProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="telegram_profile")
+    telegram_chat_id = models.CharField(max_length=100, verbose_name="Telegram Chat ID")
+    verified = models.BooleanField(default=False, verbose_name="Подтвержден")
+
+    def __str__(self):
+        return f"Telegram профиль пользователя {self.user.email}"
+
+
